36cf4d94374b43a8f5f123d26515002ad316532a
Barış Akdaş <barisakdas00@gmail.com>
2021-06-23 13:46:56 +0300

    Update index.ts
    update

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,13 @@ import tl = require("azure-pipelines-task-lib/task");
 // ## We need a function to get our job done.
 async function run() {
   try {
-    //#region You can take current branch name with following code.
-    let branchName: string | undefined = tl.getVariable(
-      "Build.SourceBranchName"
-    );
-    // //#endregion
-
-    //#region When there is a parameter we want to import for extension, you can get it with the command `getInput`.
-    let projectName: string | undefined = tl.getInput("projectName", true); // Proje adını dışardan string olarak alıyoruz.
-    //#endregion
-
-    console.log("Current Branch Name: ", branchName);
-    console.log("##Parameter - ProjectName: ", projectName);
+    let variableSample: string | undefined = tl.variableSample("Build.SourceBranchName");
+    // When there is a parameter we want to import for extension, you can get it with the command `getInput`.
+    let inputSample: string | undefined = tl.getInput("inputSample", true); 
+    
+    console.log("## input sample: ", inputSample);
+    console.log("## system variable sample: ", variableSample); // system variables 
+    
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
